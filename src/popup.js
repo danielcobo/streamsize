bd0525f4b75cfea0a441045f165ae609a352d082
danielcobo <danielcobo@users.noreply.github.com>
2022-03-15 00:38:08 +0100

    Refactor

--- src/popup.js
+++ src/popup.js
@@ -1,6 +1,3 @@
-// Initialize button with user's preferred color
-let changeColor = document.getElementById('changeColor');
-
 chrome.storage.sync.get('sizes', (data) => {
   const sizes = JSON.parse(data.sizes);
 
@@ -10,15 +7,6 @@ chrome.storage.sync.get('sizes', (data) => {
     <li data-width="${size.width}" data-height="${size.height}">${size.name}</li>`;
   });
   document.getElementById('sizes').innerHTML = html;
-
-  // console.log(
-  //   chrome.system.display.getInfo(function (displayInfo) {
-  //     displayInfo.forEach(function (info) {
-  //       console.log(info.bounds.width, info.bounds.height, info);
-  //       //keep apect ratio
-  //     });
-  //   })
-  // );
 });
 
 /**
@@ -40,59 +28,46 @@ function getLI(target) {
   }
 }
 
+/**
+ * Get scale multiplier for given
+ * width & height based on display constraint
+ * @param {DisplayInfo} displayInfo - browser API display object
+ * @param {number} width - desired width
+ * @param {number} height - desired height
+ * @returns {number} scale multiplier
+ */
+const getScale = function getScale(displayInfo, width, height) {
+  let maxHeight = 0;
+  let maxWidth = 0;
+  displayInfo.forEach(function (info) {
+    const screenWidth = info.bounds.width;
+    const screenHeight = info.bounds.height;
+    if (screenWidth > maxWidth) {
+      maxWidth = screenWidth;
+    }
+    if (screenHeight > maxHeight) {
+      maxHeight = screenHeight;
+    }
+  });
+  return 1 / Math.max(width / maxWidth, height / maxHeight, 1);
+};
+
 document.addEventListener('pointerdown', function (e) {
   const $li = getLI(e.target);
   if ($li) {
     let height = parseInt($li.dataset.height, 10);
     let width = parseInt($li.dataset.width, 10);
 
-    let maxHeight = 0;
-    let maxWidth = 0;
     chrome.system.display.getInfo(function (displayInfo) {
-      displayInfo.forEach(function (info) {
-        const screenWidth = info.bounds.width;
-        const screenHeight = info.bounds.height;
-        if (screenWidth > maxWidth) {
-          maxWidth = screenWidth;
-        }
-        if (screenHeight > maxHeight) {
-          maxHeight = screenHeight;
-        }
-      });
-      let scale = 1 / Math.max(width / maxWidth, height / maxHeight, 1);
-
+      const scale = getScale(displayInfo, width, height);
       chrome.windows.getCurrent(function (window) {
         var updateInfo = {
           width: width * scale,
           height: height * scale,
           state: 'normal',
         };
-
         chrome.windows.update(window.id, updateInfo);
       });
     });
   }
 });
-
-// // When the button is clicked, inject setPageBackgroundColor into current page
-// changeColor.addEventListener('click', async () => {
-//   let [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
-
-//   chrome.scripting.executeScript({
-//     target: { tabId: tab.id },
-//     function: setPageBackgroundColor,
-//   });
-
-//   chrome.windows.update(chrome.windows.WINDOW_ID_CURRENT, {
-//     width: 1000, //1920,
-//     height: 1080,
-//   });
-// });
-
-// The body of this function will be executed as a content script inside the
-// current page
-function setPageBackgroundColor() {
-  chrome.storage.sync.get('color', ({ color }) => {
-    document.body.style.backgroundColor = color;
-  });
-}
